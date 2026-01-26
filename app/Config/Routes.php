5908ef9e15738e935c1748c7dfa6f0cf7f08f4f6
solvam <alumno@solvam.local>
2026-01-26 09:36:01 +0100

    todos los models y controllers van

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,6 +12,9 @@ $routes->resource('material', ['controller' => 'MaterialController']);
 $routes->resource('project', ['controller' => 'ProjectsController']);
 $routes->resource('users', ['controller' => 'UsersController']);
 $routes->resource('equipment', ['controller' => 'EquipmentController']);
+$routes->resource('assigned_projects', ['controller' => 'AssignedProjectsController']);
+$routes->resource('assigned_materials', ['controller' => 'AssignedMaterialsController']);
+$routes->resource('assigned_equipment', ['controller' => 'AssignedEquipmentController']);
 
 
 //$routes->get('/clients', 'ClientsController::index');
---
+++ app/Controllers/AssignedMaterialsController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\RESTful\ResourceController;
+use App\Models\AssignedMaterialsModel;
+
+//use function PHPUnit\Framework\returnArgument;
+
+class AssignedMaterialsController extends ResourceController
+{
+    //protected $modelName = "App\Models\A_projectsModel";
+
+    protected $modelName = AssignedMaterialsModel::class;
+
+    protected $format = "json";
+    /**
+     * Return an array of resource objects, themselves in array format.
+     *
+     * @return ResponseInterface
+     */
+    public function index()
+    {
+        /* $A_material = $this->model->findAll();
+        return $this->respond($A_material); */
+        return $this->respond($this->model->getAllExpanded());
+
+
+    }
+
+    /**
+     * Return the properties of a resource object.
+     *
+     * @param int|string|null $id
+     *
+     * @return ResponseInterface
+     */
+    public function show($id = null)
+    {
+        $row = $this->model->getOneExpanded($id);
+
+        if ($row) {
+            return $this->respond($row);
+        }
+        return $this->failNotFound("Material asignado no encontrado");
+    }
+
+
+
+    /**
+     * Create a new resource object, from "posted" parameters.
+     *
+     * @return ResponseInterface
+     */
+    public function create()
+    {
+        $data = $this->request->getJSON(true);
+
+        if ($this->model->insert($data)) {
+            return $this->respondCreated($data, 'Material asignado creado.');
+        }
+
+        return $this->failValidationErrors($this->model->errors());
+
+    }
+
+
+    /**
+     * Add or update a model resource, from "posted" properties.
+     *
+     * @param int|string|null $id
+     *
+     * @return ResponseInterface
+     */
+    public function update($id = null)
+    {
+        $A_material = $this->model->find($id);
+        if (!$A_material) {
+            return $this->failNotFound('Material asignado no encontrado');
+        }
+        $data = $this->request->getJSON(true);
+        if ($this->model->update($id, $data)) {
+            return $this->respondUpdated($data, 'Material asignado actualizado.');
+        }
+        return $this->failValidationErrors($this->model->errors());
+    }
+
+    /**
+     * Delete the designated resource object from the model.
+     *
+     * @param int|string|null $id
+     *
+     * @return ResponseInterface
+     */
+    public function delete($id = null)
+    {
+        $A_material = $this->model->find($id);
+        if ($A_material) {
+            $this->model->delete($id);
+            return $this->respondDeleted($A_material, 'Material asignado elimindo.');
+        }
+        return $this->failNotFound('Material asignado no encontrado.');
+
+    }
+}
